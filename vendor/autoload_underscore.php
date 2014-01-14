a31c2178ab4fe80b47c9c0ea5d9a3202de0b461c
Joani COUMA <joani.couma@gmail.com>
2014-01-14 14:56:47 +0100

    Exo 3

--- vendor/autoload_underscore.php
+++ vendor/autoload_underscore.php
@@ -1,3 +1,9 @@
 <?php
 
 /* register autoloader here */
+
+$closure = function($className) {
+    $trueClassName = str_replace('_', '/', $className);
+    require_once __DIR__ . DIRECTORY_SEPARATOR . $trueClassName . '.php';
+};
+spl_autoload_register($closure);
